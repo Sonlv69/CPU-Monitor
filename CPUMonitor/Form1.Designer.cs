76eba2a9b064afb11dda2ec39c9a8e9f845651ea
kiluss2 <kiluss2@gmail.com>
2022-05-05 19:53:53 +0700

    push

---
+++ CPUMonitor/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace CPUMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Package");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Utilization               Current      |    Min      |     Max", new System.Windows.Forms.TreeNode[] {
+            treeNode7});
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Temperature");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("CPU name", new System.Windows.Forms.TreeNode[] {
+            treeNode8,
+            treeNode9});
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("RAM                              Current      |    Min");
+            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("Machine", new System.Windows.Forms.TreeNode[] {
+            treeNode10,
+            treeNode11});
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblContent = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.cpuProgressBarLb = new System.Windows.Forms.Label();
+            this.progressBarCPU = new System.Windows.Forms.ProgressBar();
+            this.cpuProgressBarCPUPLb = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBarRAM = new System.Windows.Forms.ProgressBar();
+            this.progressBarRAMPLb = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblContent
+            // 
+            this.lblContent.AutoSize = true;
+            this.lblContent.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContent.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblContent.Location = new System.Drawing.Point(239, 4);
+            this.lblContent.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblContent.Name = "lblContent";
+            this.lblContent.Size = new System.Drawing.Size(55, 21);
+            this.lblContent.TabIndex = 0;
+            this.lblContent.Text = "isNull";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 2000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Font = new System.Drawing.Font("Microsoft New Tai Lue", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeView1.LineColor = System.Drawing.Color.Red;
+            this.treeView1.Location = new System.Drawing.Point(12, 28);
+            this.treeView1.Name = "treeView1";
+            treeNode7.Name = "cpuUtilizationPackage";
+            treeNode7.Text = "Package";
+            treeNode8.ImageIndex = -2;
+            treeNode8.Name = "cpuUtilization";
+            treeNode8.NodeFont = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            treeNode8.Text = "Utilization               Current      |    Min      |     Max";
+            treeNode9.Name = "Temperature";
+            treeNode9.Text = "Temperature";
+            treeNode10.ImageIndex = -2;
+            treeNode10.Name = "cpuName";
+            treeNode10.Text = "CPU name";
+            treeNode11.Name = "RAM";
+            treeNode11.NodeFont = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            treeNode11.Text = "RAM                              Current      |    Min";
+            treeNode12.ImageIndex = -2;
+            treeNode12.Name = "machineName";
+            treeNode12.Text = "Machine";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode12});
+            this.treeView1.ShowRootLines = false;
+            this.treeView1.Size = new System.Drawing.Size(576, 372);
+            this.treeView1.TabIndex = 1;
+            // 
+            // cpuProgressBarLb
+            // 
+            this.cpuProgressBarLb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cpuProgressBarLb.AutoSize = true;
+            this.cpuProgressBarLb.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cpuProgressBarLb.Location = new System.Drawing.Point(12, 414);
+            this.cpuProgressBarLb.Name = "cpuProgressBarLb";
+            this.cpuProgressBarLb.Size = new System.Drawing.Size(33, 15);
+            this.cpuProgressBarLb.TabIndex = 2;
+            this.cpuProgressBarLb.Text = "CPU";
+            // 
+            // progressBarCPU
+            // 
+            this.progressBarCPU.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.progressBarCPU.Location = new System.Drawing.Point(51, 406);
+            this.progressBarCPU.Name = "progressBarCPU";
+            this.progressBarCPU.Size = new System.Drawing.Size(509, 23);
+            this.progressBarCPU.TabIndex = 3;
+            // 
+            // cpuProgressBarCPUPLb
+            // 
+            this.cpuProgressBarCPUPLb.AutoSize = true;
+            this.cpuProgressBarCPUPLb.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cpuProgressBarCPUPLb.Location = new System.Drawing.Point(565, 414);
+            this.cpuProgressBarCPUPLb.Name = "cpuProgressBarCPUPLb";
+            this.cpuProgressBarCPUPLb.Size = new System.Drawing.Size(18, 15);
+            this.cpuProgressBarCPUPLb.TabIndex = 4;
+            this.cpuProgressBarCPUPLb.Text = "%";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBarRAM
+            // 
+            this.progressBarRAM.Location = new System.Drawing.Point(50, 437);
+            this.progressBarRAM.Name = "progressBarRAM";
+            this.progressBarRAM.Size = new System.Drawing.Size(509, 23);
+            this.progressBarRAM.TabIndex = 5;
+            // 
+            // progressBarRAMPLb
+            // 
+            this.progressBarRAMPLb.AutoSize = true;
+            this.progressBarRAMPLb.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.progressBarRAMPLb.Location = new System.Drawing.Point(565, 443);
+            this.progressBarRAMPLb.Name = "progressBarRAMPLb";
+            this.progressBarRAMPLb.Size = new System.Drawing.Size(18, 15);
+            this.progressBarRAMPLb.TabIndex = 4;
+            this.progressBarRAMPLb.Text = "%";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 443);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "RAM";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 469);
+            this.Controls.Add(this.progressBarRAM);
+            this.Controls.Add(this.progressBarRAMPLb);
+            this.Controls.Add(this.cpuProgressBarCPUPLb);
+            this.Controls.Add(this.progressBarCPU);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cpuProgressBarLb);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.lblContent);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Monitor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblContent;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label cpuProgressBarLb;
+        private System.Windows.Forms.ProgressBar progressBarCPU;
+        private System.Windows.Forms.Label cpuProgressBarCPUPLb;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBarRAM;
+        private System.Windows.Forms.Label progressBarRAMPLb;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CPUMonitor/Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenHardwareMonitor.Hardware;
+using System.Collections;
+using System.Diagnostics;
+using System.Reflection;
+using System.Security.Principal;
+
+
+namespace CPUMonitor
+{
+    public partial class Form1 : Form
+    {
+        Computer thisComputer;
+        int cpuCoresNum;
+        Hashtable minMax;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Initialize open hardware monitor object
+            thisComputer = new Computer() 
+            { 
+                CPUEnabled = true, 
+                //GPUEnabled = true, 
+                RAMEnabled = true
+            };
+            minMax = new Hashtable(); //create hash table to storage min, max values
+            thisComputer.Open();
+
+            lblContent.Anchor = (AnchorStyles.Top);
+            cpuProgressBarLb.Anchor = (AnchorStyles.Left | AnchorStyles.Bottom);
+            progressBarCPU.Anchor = (cpuProgressBarLb.Anchor);
+            cpuProgressBarCPUPLb.Anchor = (AnchorStyles.Right| AnchorStyles.Bottom);
+            progressBarCPU.ForeColor = Color.BlueViolet;
+            progressBarRAM.Anchor = (AnchorStyles.Bottom | AnchorStyles.Left);
+            progressBarRAMPLb.Anchor = (AnchorStyles.Bottom | AnchorStyles.Right);
+           
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //lblContent.Text = String.Format("CPU =");
+            lblContent.Text = "System Monitor";
+            //lblContent.Text = thisComputer.Hardware[0].Sensors.Count().ToString();
+            //lblContent.Text = Environment.ProcessorCount.ToString();
+            cpuCoresNum = Environment.ProcessorCount;
+            progressBarCPU.Maximum = 100;
+            progressBarCPU.Minimum = 0;
+            FillTreeView();
+            int i = 0;
+            foreach (var node in Collect(treeView1.Nodes))
+            {
+                // load every child node here
+                if (i % 2 == 0)
+                {
+                    node.BackColor = Color.FromArgb(240, 240, 240);
+                }
+                i++;
+            }
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            try //reduce flicker??
+            {
+                treeView1.BeginUpdate();
+
+                // Update tree view
+
+                //get hardwares info
+                foreach (IHardware hw in thisComputer.Hardware)
+                {
+                    hw.Update();
+                    //CPU
+                    if (hw.HardwareType == HardwareType.CPU)
+                    {                       
+                        foreach (IHardware subHardware in hw.SubHardware)
+                            subHardware.Update();
+                        foreach (var sensor in hw.Sensors)
+                        {
+                            //update cpu package value
+                            if (sensor.SensorType == SensorType.Load)
+                            {
+                                //update min, max values
+                                if (float.Parse((string)minMax["CPUTotalMin"]) > sensor.Value.GetValueOrDefault())
+                                {
+                                    minMax["CPUTotalMin"] = sensor.Value.GetValueOrDefault().ToString();
+                                }
+                                else if (float.Parse((string)minMax["CPUTotalMax"]) < sensor.Value.GetValueOrDefault())
+                                {
+                                    minMax["CPUTotalMax"] = sensor.Value.GetValueOrDefault().ToString();
+                                }
+                                //update node text
+                                treeView1.Nodes[0].Nodes[0].Nodes[0].Nodes[0].Text = String.Format("{0:-10}:{1,14:0}%{2,15:0}%{3,15:0}%"
+                                        , sensor.Name
+                                        , sensor.Value.GetValueOrDefault()
+                                        , float.Parse((string)minMax["CPUTotalMin"])
+                                        , float.Parse((string)minMax["CPUTotalMax"])
+                                        );
+                                cpuProgressBarCPUPLb.Text = String.Format("{0:0}%", sensor.Value.GetValueOrDefault());
+                                progressBarCPU.Value = (int)sensor.Value.GetValueOrDefault();
+                            }
+
+                            //update cpu cores value
+                            for (int i = 1; i <= cpuCoresNum; i++)
+                            {
+                                if (sensor.SensorType == SensorType.Load && sensor.Name.Contains("CPU Core #" + i))
+                                {
+                                    //update min, max values
+                                    if (float.Parse((string)minMax["CPUMin" + i]) > sensor.Value.GetValueOrDefault())
+                                    {
+                                        minMax["CPUMin" + i] = sensor.Value.GetValueOrDefault().ToString();
+                                    }
+                                    else if (float.Parse((string)minMax["CPUMax" + i]) < sensor.Value.GetValueOrDefault())
+                                    {
+                                        minMax["CPUMax" + i] = sensor.Value.GetValueOrDefault().ToString();
+                                    }
+                                    //update node text
+                                    treeView1.Nodes[0].Nodes[0].Nodes[0].Nodes[i].Text = String.Format("{0:-10}:{1,9:0}%{2,15:0}%{3,15:0}%"
+                                        , sensor.Name
+                                        , sensor.Value.GetValueOrDefault()
+                                        , float.Parse((string)minMax["CPUMin" + i])
+                                        , float.Parse((string)minMax["CPUMax" + i]));
+                                }
+                            }
+
+                            //update cpu temperature
+                            if (sensor.SensorType == SensorType.Temperature)
+                            {
+                                //update min, max values
+                                if (float.Parse((string)minMax["CPUTemperatureMin"]) > sensor.Value.GetValueOrDefault())
+                                {
+                                    minMax["CPUTemperatureMin"] = sensor.Value.GetValueOrDefault().ToString();
+                                }
+                                else if (float.Parse((string)minMax["CPUTemperatureMax"]) < sensor.Value.GetValueOrDefault())
+                                {
+                                    minMax["CPUTemperatureMax"] = sensor.Value.GetValueOrDefault().ToString();
+                                }
+                                //update node text
+                                treeView1.Nodes[0].Nodes[0].Nodes[1].Nodes[0].Text = String.Format("{0:-10}:{1,9:0}°C{2,13:0}°C{3,12:0}°C"
+                                    , sensor.Name
+                                    , sensor.Value.GetValueOrDefault()
+                                    , float.Parse((string)minMax["CPUTemperatureMin"])
+                                    , float.Parse((string)minMax["CPUTemperatureMax"]));
+                            }
+                        }
+                    }
+
+                    //RAM
+                    if (hw.HardwareType == HardwareType.RAM)
+                    {
+                        foreach (IHardware subHardware in hw.SubHardware)
+                            subHardware.Update();
+                        foreach (var sensor in hw.Sensors)
+                        {
+                            //update RAM value
+                            if (sensor.SensorType == SensorType.Load)
+                            {
+                                //Update ram's minimum value
+                                if (float.Parse((string)minMax["RAMMin"]) > sensor.Value.GetValueOrDefault())
+                                {
+                                    minMax["RAMMin"] = sensor.Value.GetValueOrDefault().ToString();
+                                }
+                                //update node text
+                                treeView1.Nodes[0].Nodes[1].Nodes[0].Text = String.Format("{0:-2}:{1,23:0}%{2,15:0}%"
+                                        , sensor.Name
+                                        , sensor.Value.GetValueOrDefault()
+                                        , float.Parse((string)minMax["RAMMin"])
+                                        );
+                                progressBarRAM.Value = (int)sensor.Value.GetValueOrDefault();
+                                progressBarRAMPLb.Text = String.Format("{0:0}%", sensor.Value.GetValueOrDefault());
+                            }
+                            //update Used Memory
+                            if (sensor.SensorType == SensorType.Data && sensor.Name.Contains("Used Memory"))
+                            {                               
+                                //update node text
+                                treeView1.Nodes[0].Nodes[1].Nodes[1].Text = String.Format("{0}:  {1:0.0} GB"
+                                        , sensor.Name
+                                        , sensor.Value.GetValueOrDefault()
+                                        );
+                            }
+                        }
+                    }
+                }              
+            }
+            finally
+            {
+                treeView1.EndUpdate();
+            }
+        }
+
+        // set treeview
+        private void FillTreeView()
+        {
+            treeView1.Anchor = (progressBarCPU.Anchor | AnchorStyles.Right | AnchorStyles.Left | lblContent.Anchor);
+            // Load the images in an ImageList.
+            ImageList myImageList = new ImageList();
+            myImageList.Images.Add(Properties.Resources.item);
+            myImageList.Images.Add(Properties.Resources.computer_monitor);
+            myImageList.Images.Add(Properties.Resources.computer_cpu);
+            myImageList.Images.Add(Properties.Resources.graph);
+            myImageList.Images.Add(Properties.Resources.temperature);
+            myImageList.Images.Add(Properties.Resources.ram);
+
+           
+            // Assign the ImageList to the TreeView.
+            treeView1.ImageList = myImageList;
+
+            // Set the TreeView control's default image and selected image indexes.
+            treeView1.ImageIndex = 0;
+            treeView1.SelectedImageIndex = 0;
+
+            //create default nodes
+            //device name
+            treeView1.Nodes[0].Text = Environment.MachineName.ToString();
+            treeView1.Nodes[0].ImageIndex = 1;
+            treeView1.Nodes[0].SelectedImageIndex = 1;
+            //cpu name
+            treeView1.Nodes[0].Nodes[0].Text = thisComputer.Hardware[0].Name.ToString();
+            treeView1.Nodes[0].Nodes[0].ImageIndex = 2;
+            treeView1.Nodes[0].Nodes[0].SelectedImageIndex = 2;
+            //cpu Usage
+            treeView1.Nodes[0].Nodes[0].Nodes[0].ImageIndex = 3;
+            treeView1.Nodes[0].Nodes[0].Nodes[0].SelectedImageIndex = 3;
+            //cpu temp
+            treeView1.Nodes[0].Nodes[0].Nodes[1].ImageIndex = 4;
+            treeView1.Nodes[0].Nodes[0].Nodes[1].SelectedImageIndex = 4;
+
+            String currentVal;
+            float usedMem = 0;
+            //add items to tree view
+            foreach (IHardware hw in thisComputer.Hardware)
+            {
+                hw.Update();
+                //create CPU nodes, min, max values
+                if (hw.HardwareType == HardwareType.CPU)
+                {
+                    foreach (var sensor in hw.Sensors)
+                    {
+                        //cpu total
+                        if (sensor.SensorType == SensorType.Load && sensor.Name.Contains("CPU Total"))
+                        {
+                            currentVal = sensor.Value.GetValueOrDefault().ToString();
+                            minMax.Add("CPUTotalMin", currentVal);
+                            minMax.Add("CPUTotalMax", currentVal);
+
+                        }
+                        //add all cpu cores
+                        for (int i = 1; i <= cpuCoresNum; i++)
+                        {
+                            if (sensor.SensorType == SensorType.Load && sensor.Name.Contains("CPU Core #" + i))
+                            {
+                                currentVal = sensor.Value.GetValueOrDefault().ToString();
+                                treeView1.Nodes[0].Nodes[0].Nodes[0].Nodes.Add("initializing");
+                                minMax.Add("CPUMin" + i, currentVal);
+                                minMax.Add("CPUMax" + i, currentVal);
+                            }
+                        }
+                        //cpu temperature
+                        if (sensor.SensorType == SensorType.Temperature && sensor.Name.Contains("CPU Package"))
+                        {
+                            currentVal = sensor.Value.GetValueOrDefault().ToString();
+                            treeView1.Nodes[0].Nodes[0].Nodes[1].Nodes.Add("initializing");
+                            minMax.Add("CPUTemperatureMin", currentVal);
+                            minMax.Add("CPUTemperatureMax", currentVal);
+                        }
+                    }
+                }
+
+                //create RAM nodes, min, max values
+                if (hw.HardwareType == HardwareType.RAM)
+                {
+                    foreach (var sensor in hw.Sensors)
+                    {
+                        //Ram percent
+                        if (sensor.SensorType == SensorType.Load)
+                        {
+                            treeView1.Nodes[0].Nodes[1].Nodes.Add("initializing");
+                            currentVal = sensor.Value.GetValueOrDefault().ToString();
+                            minMax.Add("RAMMin", currentVal);
+                        }
+                        //ram values
+                        if (sensor.SensorType == SensorType.Data)
+                        {
+                            if (sensor.Name.Contains("Used Memory"))
+                            {
+                                usedMem = sensor.Value.GetValueOrDefault();
+                                treeView1.Nodes[0].Nodes[1].Nodes.Add("initializing");
+                                
+                            }
+                            if (sensor.Name.Contains("Available Memory"))
+                            {
+                                // caculate total memory
+                                float totalMem = usedMem + sensor.Value.GetValueOrDefault(); 
+                                treeView1.Nodes[0].Nodes[1].Nodes.Add(String.Format("Total memory:  {0:0.0} GB",totalMem));
+                            }
+                        }
+
+                    }
+                }
+            }
+
+
+            treeView1.ExpandAll();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBarCPU.Width = this.Width - 110;
+            progressBarRAM.Width = this.Width - 110;
+        }
+
+        IEnumerable<TreeNode> Collect(TreeNodeCollection nodes)
+        {
+            foreach (TreeNode node in nodes)
+            {
+                yield return node;
+
+                foreach (var child in Collect(node.Nodes))
+                    yield return child;
+            }
+        }
+    }
+
+    
+}
